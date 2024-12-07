d5bdc9cb25829c94b94b1d9e134cb69ff2e25174
memento <claude.rc@gmail.com>
2024-12-07 17:27:29 -0600

    (ref) updates from passing conda-staged recipes

--- recipes/serial-monitor-rust/build.sh
+++ recipes/serial-monitor-rust/build.sh
@@ -3,9 +3,9 @@
 set -euxo pipefail
 
 # Use Conda Rust source libraries (compiled/tested) instead of downloading from crates.io
-EGUI_PHOSPHOR_VERSION=$(ls $BUILD_PREFIX/src/rust-libraries/egui-phosphor-rust-source-* -d | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
-PREFERENCES_VERSION=$(ls $BUILD_PREFIX/src/rust-libraries/preferences-rs-rust-source-* -d | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
-SERIALPORT_VERSION=$(ls $BUILD_PREFIX/src/rust-libraries/serialport-rs-rust-source-* -d | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
+EGUI_PHOSPHOR_VERSION=$(find $BUILD_PREFIX/src/rust-libraries/egui-phosphor-rust-source-* -type d -exec basename {} \; | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
+PREFERENCES_VERSION=$(find $BUILD_PREFIX/src/rust-libraries/preferences-rs-rust-source-* -type d -exec basename {} \; | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
+SERIALPORT_VERSION=$(find $BUILD_PREFIX/src/rust-libraries/serialport-rs-rust-source-* -type d -exec basename {} \; | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
 
 mkdir -p .cargo
 touch .cargo/config.toml
@@ -16,6 +16,12 @@ serialport = { path = "${BUILD_PREFIX}/src/rust-libraries/serialport-rs-rust-sou
 egui-phosphor = { path = "${BUILD_PREFIX}/src/rust-libraries/egui-phosphor-rust-source-${EGUI_PHOSPHOR_VERSION}" }
 preferences = { path = "${BUILD_PREFIX}/src/rust-libraries/preferences-rs-rust-source-${PREFERENCES_VERSION}" }
 EOF
+
+if [[ "${target_platform}" == osx-* ]]; then
+  export PATH="/Applications/Xcode_15.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin:$PATH"
+  export CPATH="/Applications/Xcode_15.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
+  unset CONDA_BUILD_SYSROOT
+fi
 
 cargo build --release --all-targets
 cargo test --release --all-targets
