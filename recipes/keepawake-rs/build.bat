e0e0ed73a9e48f0bf3a8937270df4bc1028b4a4b
memento <claude.rc@gmail.com>
2024-12-11 11:54:30 -0600

    (ref) v0.3.2 win: CARGO_HOME?

--- recipes/keepawake-rs/build.bat
+++ recipes/keepawake-rs/build.bat
@@ -1,6 +1,17 @@
 @echo off
 
-call cargo build --release --all-targets
-call cargo test --release --all-targets
-call cargo install --path . --root "%PREFIX%" --features="bin"
-call cargo-bundle-licenses --format yaml --output "%RECIPE_DIR%\THIRDPARTY.yml"
+set CARGO_HOME=%BUILD_PREFIX%\cargo
+mkdir %CARGO_HOME%
+icacls %CARGO_HOME% /grant Users:F
+
+cargo build --release --all-targets
+if %errorlevel% NEQ 0 exit /b %errorlevel%
+
+cargo test --release --all-targets
+if %errorlevel% NEQ 0 exit /b %errorlevel%
+
+cargo install --path . --root "%PREFIX%" --features="bin"
+if %errorlevel% NEQ 0 exit /b %errorlevel%
+
+cargo-bundle-licenses --format yaml --output "%RECIPE_DIR%\THIRDPARTY.yml"
+if %errorlevel% NEQ 0 exit /b %errorlevel%
