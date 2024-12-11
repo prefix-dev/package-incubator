cf9fa3e4b1acabe55d04db4d58c6caacb0f192e8
memento <claude.rc@gmail.com>
2024-12-11 14:43:34 -0600

    (ref) osx: MACOS_DEPLOY + time

--- recipes/keepawake-rs/build.sh
+++ recipes/keepawake-rs/build.sh
@@ -2,6 +2,18 @@
 
 set -euxo pipefail
 
+export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
+export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
+
+BINDGEN_EXTRA_CLANG_ARGS="-v ${CPPFLAGS} ${CFLAGS}"
+if [[ "${target_platform}" == osx-arm64 ]]; then
+    BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS} --target=aarch64-apple-darwin"
+else
+    BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS} --target=x86_64-apple-darwin13.4.0"
+fi
+export BINDGEN_EXTRA_CLANG_ARGS="$(echo "${BINDGEN_EXTRA_CLANG_ARGS}" | sed 's/ /\n/g' | sort -u | paste -s -d ' ' -)"
+export LIBCLANG_PATH=${BUILD_PREFIX}/lib
+
 cargo build --release --all-targets
 # Skip doc-test which fails to find cc as a linker (odd, since it can build just fine)
 cargo test --release --all-targets
