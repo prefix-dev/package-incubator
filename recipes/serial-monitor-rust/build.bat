afb7c0022cafac75e62c59d7c0d7317b9df2adae
memento <claude.rc@gmail.com>
2024-12-11 18:31:34 -0600

    (ref) win: add early fails. add recipe tests

--- recipes/serial-monitor-rust/build.bat
+++ recipes/serial-monitor-rust/build.bat
@@ -1,6 +1,12 @@
 @echo off
 
-call cargo build --release --all-targets
-call cargo test --release --all-targets
-call cargo install --path . --root "%PREFIX%" --features="bin"
-call cargo-bundle-licenses --format yaml --output "%RECIPE_DIR%\THIRDPARTY.yml"
+cargo build --release --all-targets
+if %errorlevel% NEQ 0 exit /b %errorlevel%
+
+cargo test --release --all-targets
+if %errorlevel% NEQ 0 exit /b %errorlevel%
+
+cargo install --path . --root "%PREFIX%" --features="bin"
+if %errorlevel% NEQ 0 exit /b %errorlevel%
+
+cargo-bundle-licenses --format yaml --output "%RECIPE_DIR%\THIRDPARTY.yml"
