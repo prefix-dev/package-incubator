32cad4b6e8868f4f623e737a8f16d7dfc0727583
memento <claude.rc@gmail.com>
2024-12-11 18:00:56 -0600

    (ref) osx: serail same as keepawake

--- recipes/keepawake-rs/build.sh
+++ recipes/keepawake-rs/build.sh
@@ -6,12 +6,14 @@ export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
 export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
 
 BINDGEN_EXTRA_CLANG_ARGS="-v ${CPPFLAGS} ${CFLAGS}"
-if [[ "${target_platform}" == osx-arm64 ]]; then
-    BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS} --target=aarch64-apple-darwin"
-else
-    BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS} --target=x86_64-apple-darwin13.4.0"
+if [[ "${target_platform}" == osx-* ]]; then
+  if [[ "${target_platform}" == osx-arm64 ]]; then
+      BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS} --target=aarch64-apple-darwin"
+  else
+      BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS} --target=x86_64-apple-darwin13.4.0"
+  fi
+  export LIBCLANG_PATH=${BUILD_PREFIX}/lib
 fi
-export LIBCLANG_PATH=${BUILD_PREFIX}/lib
 
 cargo build --release --all-targets
 # Skip doc-test which fails to find cc as a linker (odd, since it can build just fine)
--- recipes/serial-monitor-rust/build.sh
+++ recipes/serial-monitor-rust/build.sh
@@ -2,6 +2,19 @@
 
 set -euxo pipefail
 
+export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
+export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
+
+BINDGEN_EXTRA_CLANG_ARGS="-v ${CPPFLAGS} ${CFLAGS}"
+if [[ "${target_platform}" == osx-* ]]; then
+  if [[ "${target_platform}" == osx-arm64 ]]; then
+      BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS} --target=aarch64-apple-darwin"
+  else
+      BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS} --target=x86_64-apple-darwin13.4.0"
+  fi
+  export LIBCLANG_PATH=${BUILD_PREFIX}/lib
+fi
+
 cargo build --release --all-targets
 cargo test --release --all-targets
 CARGO_TARGET_DIR=target cargo install --path . --root "${PREFIX}"
