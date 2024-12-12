af8499c9b57bc6739e7f2238d7f798c07da287e9
memento <claude.rc@gmail.com>
2024-12-11 21:24:43 -0600

    (ref) win: compiles but can't install? Let's not test a GUI ... lol

--- recipes/serial-monitor-rust/build.bat
+++ recipes/serial-monitor-rust/build.bat
@@ -1,12 +1,13 @@
 @echo off
 
-cargo build --release --all-targets
+cargo build --release --all-targets --verbose
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 
-cargo test --release --all-targets
+cargo test --release --all-targets --verbose
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 
-cargo install --path . --root "%PREFIX%" --features="bin"
+set CARGO_TARGET_DIR=target
+cargo install --path . --root "%PREFIX%" --verbose
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 
 cargo-bundle-licenses --format yaml --output "%RECIPE_DIR%\THIRDPARTY.yml"
