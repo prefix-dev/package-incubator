1351bd610cd411b9a0ff60457c6d9d07de55dc19
memento <claude.rc@gmail.com>
2025-02-01 10:16:58 -0600

    (ref) arch. removed several patches

--- recipes/renode-cores/build.bat
+++ recipes/renode-cores/build.bat
@@ -3,11 +3,28 @@
 setlocal EnableDelayedExpansion
 
 rem Update the submodule to the latest commit CMakeLists.txt
-copy "%RECIPE_DIR%\patches\Cores-CMakeLists.txt" "%SRC_DIR%\src\Infrastructure\src\Emulator\Cores\CMakeLists.txt"
-copy "%RECIPE_DIR%\patches\tlib-CMakeLists.txt" "%SRC_DIR%\src\Infrastructure\src\Emulator\Cores\tlib\CMakeLists.txt"
-if %errorlevel% neq 0 exit /b  %errorlevel%
+mkdir %SRC_DIR%\updates
+pushd %SRC_DIR%\updates
+    git clone "https://github.com/renode/renode-infrastructure.git"
+    pushd renode-infrastructure
+        git checkout 3fc2d5fe643068e595e875d9408cb4329522b229
+        set "CMAKEFILES_TXT=src\Emulator\Cores\CMakeLists.txt"
+        copy %CMAKEFILES_TXT% "%SRC_DIR%\src\Infrastructure\%CMAKEFILES_TXT%"
+    popd
 
-powershell -ExecutionPolicy Bypass -File "%RECIPE_DIR%\helpers\renode_build_with_cmake.ps1" --tlib-only --net --no-gui
+    git clone "https://github.com/antmicro/tlib.git"
+    pushd tlib
+        git checkout 69fff75a0eba7471283b0b8db2e55e8388e284f6
+        copy "CMakeLists.txt" "%SRC_DIR%\src\Infrastructure\src\Emulator\Cores\tlib"
+        copy "tcg\CMakeLists.txt" "%SRC_DIR%\src\Infrastructure\src\Emulator\Cores\tlib\tcg"
+        copy LICENSE %RECIPE_DIR%\tlib-LICENSE
+    popd
+    if %errorlevel% neq 0 exit /b  %errorlevel%
+popd
+
+copy "%SRC_DIR%\src\Infrastructure\src\Emulator\Cores\tlib\softfloat-3\COPYING.txt" "%RECIPE_DIR%\softfloat-3-COPYING.txt"
+
+powershell -ExecutionPolicy Bypass -File "%RECIPE_DIR%\helpers\renode_build_with_cmake.ps1"
 if %errorlevel% neq 0 exit /b  %errorlevel%
 
 rem Install procedure into a conda path that renode-cli can retrieve
@@ -20,12 +37,12 @@ icacls "%PREFIX%\Library\lib\%PKG_NAME%" /grant Users:(OI)(CI)F /T
 robocopy "%CORES_BIN_PATH%\lib" "%PREFIX%\Library\lib\%PKG_NAME%" /E /COPY:DATSO
 
 :: Setting conda host environment variables
-if not exist "%PREFIX%\etc\conda\activate.d\" mkdir "%PREFIX%\etc\conda\activate.d\"
-if not exist "%PREFIX%\etc\conda\deactivate.d\" mkdir "%PREFIX%\etc\conda\deactivate.d\"
-
-copy "%RECIPE_DIR%\scripts\activate.bat" "%PREFIX%\etc\conda\activate.d\%PKG_NAME%-activate.bat" > nul
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-copy "%RECIPE_DIR%\scripts\deactivate.bat" "%PREFIX%\etc\conda\deactivate.d\%PKG_NAME%-deactivate.bat" > nul
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+:: if not exist "%PREFIX%\etc\conda\activate.d\" mkdir "%PREFIX%\etc\conda\activate.d\"
+:: if not exist "%PREFIX%\etc\conda\deactivate.d\" mkdir "%PREFIX%\etc\conda\deactivate.d\"
+::
+:: copy "%RECIPE_DIR%\scripts\activate.bat" "%PREFIX%\etc\conda\activate.d\%PKG_NAME%-activate.bat" > nul
+:: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+:: copy "%RECIPE_DIR%\scripts\deactivate.bat" "%PREFIX%\etc\conda\deactivate.d\%PKG_NAME%-deactivate.bat" > nul
+:: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 endlocal
--- recipes/renode-cores/build.sh
+++ recipes/renode-cores/build.sh
@@ -2,12 +2,17 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# Update the submodule to the latest commit CMakeLists.txt
-cp ${RECIPE_DIR}/patches/Cores-CMakeLists.txt ${SRC_DIR}/src/Infrastructure/src/Emulator/Cores/CMakeLists.txt
-cp ${RECIPE_DIR}/patches/tlib-CMakeLists.txt ${SRC_DIR}/src/Infrastructure/src/Emulator/Cores/tlib/CMakeLists.txt
+# Update the submodule CMakeLists.txt with a recent version (post-CMake conversion)
+CMAKEFILES_TXT="src/Emulator/Cores/CMakeLists.txt"
+cp "cmake-renode-infrastructure/${CMAKEFILES_TXT}" "${SRC_DIR}/src/Infrastructure/${CMAKEFILES_TXT}"
+cp cmake-tlib/CMakeLists.txt "${SRC_DIR}/src/Infrastructure/src/Emulator/Cores/tlib"
+cp cmake-tlib/tcg/CMakeLists.txt "${SRC_DIR}/src/Infrastructure/src/Emulator/Cores/tlib/tcg"
+
+cp cmake-tlib/LICENSE "${RECIPE_DIR}/tlib-LICENSE"
+cp "${SRC_DIR}/src/Infrastructure/src/Emulator/Cores/tlib/softfloat-3/COPYING.txt" "${RECIPE_DIR}/softfloat-3-COPYING.txt"
 
 chmod +x build.sh tools/building/check_weak_implementations.sh
-${RECIPE_DIR}/helpers/renode_build_with_cmake.sh --tlib-only --net --no-gui
+${RECIPE_DIR}/helpers/renode_build_with_cmake.sh
 
 # Install procedure into a conda path that renode-cli can retrieve
 CONFIGURATION="Release"
@@ -19,8 +24,8 @@ tar -c -C "${CORES_BIN_PATH}/lib" . | tar -x -C "${PREFIX}/lib/${PKG_NAME}"
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
-for CHANGE in "activate" "deactivate"
-do
-  mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-  cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}-${CHANGE}.sh"
-done
+# for CHANGE in "activate" "deactivate"
+# do
+#   mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+#   cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}-${CHANGE}.sh"
+# done
