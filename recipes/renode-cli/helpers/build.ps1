ec17ee4ba5f381f8fa4349b885bf88d4722210bc
memento <claude.rc@gmail.com>
2025-02-02 17:43:49 -0600

    (fix) win: env vars in PS1

--- recipes/renode-cli/helpers/build.ps1
+++ recipes/renode-cli/helpers/build.ps1
@@ -11,45 +11,45 @@ if ($dotnet_version -match "^(\d+\.\d+)") {
 (Get-Content "Renode_NET.sln") | ForEach-Object { $_ -replace "(ReleaseHeadless|Any .+ = )Debug", '$1Release' } | Set-Content "Renode_NET.sln"
 
 # Prevent CMake build, copy pre-built binaries
-New-Item -ItemType Directory -Path "$SRC_DIR\src\Infrastructure\src\Emulator\Cores\bin\Release\lib" -Force
-Copy-Item -Path "$BUILD_PREFIX\Library\lib\renode-cores\*" -Destination "$SRC_DIR\src\Infrastructure\src\Emulator\Cores\bin\Release\lib" -Force
+New-Item -ItemType Directory -Path "$Env:SRC_DIR\src\Infrastructure\src\Emulator\Cores\bin\Release\lib" -Force
+Copy-Item -Path "$Env:BUILD_PREFIX\Library\lib\renode-cores\*" -Destination "$Env:SRC_DIR\src\Infrastructure\src\Emulator\Cores\bin\Release\lib" -Force
 
 # Remove C cores not built in this recipe
-Remove-Item -Path "$SRC_DIR\src\Infrastructure\src\Emulator\Cores\translate*.cproj" -Force
+Remove-Item -Path "$Env:SRC_DIR\src\Infrastructure\src\Emulator\Cores\translate*.cproj" -Force
 
 # Build with dotnet
-& $RECIPE_DIR\helpers\renode_build_with_dotnet.ps1 $framework_version
+& $Env:RECIPE_DIR\helpers\renode_build_with_dotnet.ps1 $framework_version
 
 # Install procedure
-New-Item -ItemType Directory -Path "$PREFIX\libexec\$PKG_NAME" -Force
-Copy-Item -Path "output\bin\Release\net$framework_version\*" -Destination "$PREFIX\libexec\$PKG_NAME" -Recurse -Force
+New-Item -ItemType Directory -Path "$Env:PREFIX\libexec\$Env:PKG_NAME" -Force
+Copy-Item -Path "output\bin\Release\net$framework_version\*" -Destination "$Env:PREFIX\libexec\$Env:PKG_NAME" -Recurse -Force
 
-New-Item -ItemType Directory -Path "$PREFIX\opt\$PKG_NAME\scripts", "$PREFIX\opt\$PKG_NAME\platforms", "$PREFIX\opt\$PKG_NAME\tests", "$PREFIX\opt\$PKG_NAME\tools", "$PREFIX\opt\$PKG_NAME\licenses" -Force
+New-Item -ItemType Directory -Path "$Env:PREFIX\opt\$Env:PKG_NAME\scripts", "$Env:PREFIX\opt\$Env:PKG_NAME\platforms", "$Env:PREFIX\opt\$Env:PKG_NAME\tests", "$Env:PREFIX\opt\$Env:PKG_NAME\tools", "$Env:PREFIX\opt\$Env:PKG_NAME\licenses" -Force
 
-Copy-Item -Path ".renode-root" -Destination "$PREFIX\opt\$PKG_NAME" -Force
-Copy-Item -Path "scripts\*" -Destination "$PREFIX\opt\$PKG_NAME\scripts" -Recurse -Force
-Copy-Item -Path "platforms\*" -Destination "$PREFIX\opt\$PKG_NAME\platforms" -Recurse -Force
-Copy-Item -Path "tests\*" -Destination "$PREFIX\opt\$PKG_NAME\tests" -Recurse -Force
-Copy-Item -Path "tools\metrics_analyzer", "tools\execution_tracer", "tools\gdb_compare", "tools\sel4_extensions" -Destination "$PREFIX\opt\$PKG_NAME\tools" -Recurse -Force
+Copy-Item -Path ".renode-root" -Destination "$Env:PREFIX\opt\$Env:PKG_NAME" -Force
+Copy-Item -Path "scripts\*" -Destination "$Env:PREFIX\opt\$Env:PKG_NAME\scripts" -Recurse -Force
+Copy-Item -Path "platforms\*" -Destination "$Env:PREFIX\opt\$Env:PKG_NAME\platforms" -Recurse -Force
+Copy-Item -Path "tests\*" -Destination "$Env:PREFIX\opt\$Env:PKG_NAME\tests" -Recurse -Force
+Copy-Item -Path "tools\metrics_analyzer", "tools\execution_tracer", "tools\gdb_compare", "tools\sel4_extensions" -Destination "$Env:PREFIX\opt\$Env:PKG_NAME\tools" -Recurse -Force
 
-Copy-Item "lib\resources\styles\robot.css" "$PREFIX\opt\$PKG_NAME\tests" -Force
+Copy-Item "lib\resources\styles\robot.css" "$Env:PREFIX\opt\$Env:PKG_NAME\tests" -Force
 
-& tools\packaging\common_copy_licenses.ps1 "$PREFIX\opt\$PKG_NAME\licenses" linux
-Copy-Item -Path "$PREFIX\opt\$PKG_NAME\licenses" -Destination "license-files" -Recurse -Force
+& tools\packaging\common_copy_licenses.ps1 "$Env:PREFIX\opt\$Env:PKG_NAME\licenses" linux
+Copy-Item -Path "$Env:PREFIX\opt\$Env:PKG_NAME\licenses" -Destination "license-files" -Recurse -Force
 
 # Update robot_tests_provider.py (replace path to robot.css)
-(Get-Content "$PREFIX\opt\$PKG_NAME\tests\robot_tests_provider.py") | ForEach-Object { $_ -replace "os\.path\.join\(this_path, '\.\./lib/resources/styles/robot\.css'\)", "os.path.join(this_path,'robot.css')" } | Set-Content "$PREFIX\opt\$PKG_NAME\tests\robot_tests_provider.py"
+(Get-Content "$Env:PREFIX\opt\$Env:PKG_NAME\tests\robot_tests_provider.py") | ForEach-Object { $_ -replace "os\.path\.join\(this_path, '\.\./lib/resources/styles/robot\.css'\)", "os.path.join(this_path,'robot.css')" } | Set-Content "$Env:PREFIX\opt\$Env:PKG_NAME\tests\robot_tests_provider.py"
 
 # Create renode.cmd
-New-Item -ItemType File -Path "$PREFIX\bin\renode.cmd" -Force
+New-Item -ItemType File -Path "$Env:PREFIX\bin\renode.cmd" -Force
 @"
 @echo off
 call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\renode-cli\Renode.dll %*
-"@ | Out-File -FilePath "$PREFIX\bin\renode.cmd" -Encoding ascii
+"@ | Out-File -FilePath "$Env:PREFIX\bin\renode.cmd" -Encoding ascii
 # No chmod +x needed in PowerShell
 
 # Create renode-test.cmd
-New-Item -ItemType File -Path "$PREFIX\bin\renode-test.cmd" -Force
+New-Item -ItemType File -Path "$Env:PREFIX\bin\renode-test.cmd" -Force
 @"
 @echo off
 setlocal enabledelayedexpansion
@@ -58,6 +58,6 @@ python3 "%CONDA_PREFIX%\opt\renode-cli\tests\run_tests.py" --robot-framework-rem
 set "RESULT_CODE=%ERRORLEVEL%"
 if not "%STTY_CONFIG%"=="" stty "%STTY_CONFIG%"
 exit /b %RESULT_CODE%
-"@ | Out-File -FilePath "$PREFIX\bin\renode-test.cmd" -Encoding ascii
+"@ | Out-File -FilePath "$Env:PREFIX\bin\renode-test.cmd" -Encoding ascii
 # No chmod +x needed in PowerShell
 
